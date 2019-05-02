4be7b5dbace354c574732ecfaf57f1fa3d7ffe04
xxx <vaahmerda@gmail.com>
2019-05-02 00:58:21 -0300

    cmg

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,10 +6,10 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+       <!-- <b-navbar-nav>
           <b-nav-item href="#" id="episodios">Episodios</b-nav-item>
           <b-nav-item href="#">Como assistir ?</b-nav-item>
-        </b-navbar-nav>
+        </b-navbar-nav> -->
 
         <!-- Right aligned nav items -->
       </b-collapse>
@@ -19,11 +19,11 @@
 
 <script>
 export default {
-  name: 'NavBar',
-  data(){
-      return{
-          msg: 'test'
-      }
+  name: "NavBar",
+  data() {
+    return {
+      msg: "test"
+    };
   }
 };
 </script>
